f580993d809670af98d93c1e0e785dff7bd0ceec
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2025-05-01 15:00:14 +0500

    cf 310

---
+++ D_Product_Subsequences.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#ifndef ONLINE_JUDGE
+#include "debugging.h"
+#else
+#define debug(x) 0
+#endif
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<long long, long long>>
+#define vvll vector<vector<long long>>
+#define vc vector<char>
+#define vs vector<string>
+#define vb vector<bool>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define mll map<long long, long long>
+#define umap unordered_map
+#define uset unordered_set
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fise(i, s, e) for (long long int i = s; i < e; i++)
+#define fnr(i, n) for (long long int i = 0; i < n; i++)
+#define fora(a) for(auto u:a)
+#define cf(i, s, e) for (long long int i = s; i <= e; i++)
+#define rcf(i, e, s) for (long long int i = e; i >= s; i--)
+#define fies(i, e, s) for (long long int i = e; i > s; i--)
+#define len(s) (ll)s.size()
+#define pb push_back
+#define eb emplace_back
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+
+#define isOn(S, j) (S & (1<<j))
+#define setBit(S, j) (S |= (1<<j))
+#define clearBit(S, j) (S &= ~(1<<j))
+#define toggleBit(S, j) (S ^= (1<<j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1<<n)-1)
+#define modulo(S, N) ((S) & (N-1))
+#define isPowerOfTwo(S) (!(S & (S-1)))
+#define nearestPowerOfTwoLess(S) (1<<(int)log2(S))
+#define nearestPowerOfTwoMore(S) (1<<((int)log2(S) + 1))
+#define turnOffLastBit(S) ((S) & (S-1))
+#define turnOnLastZero(S) ((S) | (S+1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S+1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S-1))
+
+/* PRINTS */
+template <class T> void print_v(vector<T> &v) { for (auto x : v) cout << x << " "; cout << "\n"; }
+template<char sep=' ', char end='\n', class T, class... U>
+void print( const T& first, const U&... rest)
+{
+    std::cout << first;
+    ((std::cout << sep << rest ), ...);
+    std::cout << end;
+}
+void print() {cout << '\n';}
+void printInt128(__int128 x) { if (x == 0) { cout << "0"; return; } string res = ""; while (x) { res += (char)('0' + (x % 10)); x /= 10; } reverse(res.begin(), res.end()); cout << res; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+
+double binom(int n, int k) { return 1/((n+1)*std::beta(n-k+1,k+1));}
+ll gcd(ll a,ll b) { return __gcd(a, b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+ll modexp(ll a,ll b,ll m) { ll res=1; while (b>0) { if (b&1) res=(res*a)%m; a=(a*a)%m; b>>=1; } return res; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+void Case(int &x) {cout << "Case " << x++ << ": ";}
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+/* All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+typedef __int128 lll;
+
+int tc = 1;
+
+ll calc_comb(int n, int k) {
+    if (k > n) return 0;
+    if (k == 0 || k == n) return 1;
+    
+    vector<ll> fact(n + 1), inv(n + 1), finv(n + 1);
+    fact[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        fact[i] = (fact[i - 1] * i) % MOD;
+    }
+    
+    inv[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        inv[i] = MOD - (MOD / i) * inv[MOD % i] % MOD;
+    }
+    
+    finv[0] = finv[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        finv[i] = (finv[i - 1] * inv[i]) % MOD;
+    }
+    
+    return (((fact[n] * finv[k]) % MOD) * finv[n - k]) % MOD;
+}
+
+void solve() {
+    int n; cin >> n;
+    int z = 0, p = 0, ng = 0;
+    ll x;
+    fnr(i, n)  {
+        cin >> x;
+        z += (x == 0);
+        p += (x > 0);
+        ng += (x < 0);
+    }
+
+    ll nz = 0, np = 0, nn = 0;
+    nz = ((1LL << n) - 1) - ((1LL << (n - z)) - 1);
+    nz = (nz % MOD + MOD) % MOD;
+    
+    if (p + ng == 0) {
+        np = 0;
+        nn = 0;
+    } else {
+        for (int i = 0; i <= ng; i++) {
+            ll neg_ways = 0;
+            if (i == 0) neg_ways = 1;
+            else neg_ways = calc_comb(ng, i);
+            
+            ll pos_ways = (1LL << p) % MOD;
+            
+            ll total = (neg_ways * pos_ways) % MOD;
+            
+            if (i % 2 == 0) np = (np + total) % MOD;
+            else nn = (nn + total) % MOD;
+        }
+        
+        np = (np - 1 + MOD) % MOD;
+    }
+    
+    print(np, nn, nz);
+}
+
+int main() {
+    // auto st = std::chrono::high_resolution_clock::now();
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t; cin >> t; while(t--)
+    solve();
+
+    // cerr << "Time measured: " << (ld)(chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - st)).count() / 1000.0 << " seconds.\n";
+
+    return 0;
+}
